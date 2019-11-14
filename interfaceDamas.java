f2e4cf7fa402a08269fcd2511d8fc7e33d96cefd
ayrton1907-art <lopezayrtonemilio@gmail.com>
2019-11-13 22:16:27 -0300

    detalles

--- interfaceDamas.java
+++ interfaceDamas.java
@@ -1,5 +1,6 @@
 interface interfaceDamas{
 
+  public  lista<Nodos> adyaNodo (Grafo graf);
   public boolean solucion();
   public void saltar();
   public void camino();
--- interfaceGrafo.java
+++ interfaceGrafo.java
@@ -1,6 +1,8 @@
 interface interfaceGrafo {
 
-  public void insFicha(Pair pos);
+  //public void insFicha(Pair pos);
+  public void insFicha(Nodo aux);
+  public void insListaNodos(lista<Nodo> lis);
   public void buscarFicha();
 
 
